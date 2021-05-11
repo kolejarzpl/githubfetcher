5b2423c9695b86ff60b0bba440579a8633a4f6b1
KrzysztofO19926 <krzysiek.19926@gmail.com>
2021-05-11 21:47:46 +0200

    some changes

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import os
 import urllib.request
 import json
+import csv
+import sys
 import pandas as pd
 
 # URL = "https://api.github.com/repos/nvbn/thefuck/contributors"
 URL = "https://api.github.com/users/KrzysztofO19926"
 
 
-# JSON_FILE_PATH = "path"
-
-
 def fetch_data(url):
     with urllib.request.urlopen(url) as response:
-        resp = json.loads(response.read())
+        resp = json.loads(response.read().decode())
         print(resp)
         return resp
 
 
 def save_data(resp):
+    #
     nameOfJsonFile = "contributors.json"
     nameOfCSVFile = "contributors.csv"
 
@@ -26,15 +26,32 @@ def save_data(resp):
     file = open(filepath, 'w+')
     file.write(str(resp))
 
-    print(resp)
+    # print(resp['login'])
 
-    # jsonFile = pd.read_json(filepath)
-    jsonFile = pd.read_json(r'C:\Users\Krzysiek\Dropbox\priv\Python\githubfetcher\contributors.json', orient='index')
+    # x = json.dumps(resp)
 
-    jsonFile.to_csv(filepath + nameOfCSVFile, index=False)
+    for key, value in resp.items():
+        print(key, value)
+
+    csv_columns = ['No', 'Name', 'Country']
+    dict_data = [
+        {'No': 1, 'Name': 'Alex', 'Country': 'India'},
+        {'No': 2, 'Name': 'Ben', 'Country': 'USA'},
+        {'No': 3, 'Name': 'Shri Ram', 'Country': 'India'},
+        {'No': 4, 'Name': 'Smith', 'Country': 'USA'},
+        {'No': 5, 'Name': 'Yuva Raj', 'Country': 'India'},
+    ]
+    csv_file = "Names.csv"
+    try:
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in dict_data:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
 
 
 if __name__ == '__main__':
     resp = fetch_data(URL)
     save_data(resp)
-
