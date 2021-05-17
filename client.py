ecad192bbbb466be727490ba9d3eb9baafa7c712
Pawel Szalawinski <pawel.szalawinski@tink.com>
2021-05-17 21:24:16 +0200

    trying sockets

---
+++ client.py
@@ -0,0 +1,14 @@
+import os
+from socket import socket, AF_INET, SOCK_DGRAM
+
+host = "192.168.1.162"  # set to IP address of target computer
+port = 13002
+addr = (host, port)
+UDPSock = socket(AF_INET, SOCK_DGRAM)
+while True:
+    data = input("Enter message to send or type 'exit': ")
+    UDPSock.sendto(data, addr)
+    if data == "exit":
+        break
+UDPSock.close()
+os._exit(0)
